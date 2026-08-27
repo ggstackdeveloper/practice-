2785c4b6ecee96e74d0b8680b192f61d4bcacc84
ggstackdeveloper <ggstackdeveloper@gmail.com>
2026-08-27 22:38:11 +0530

    first commit

---
+++ practicecode/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="container">
+        <h1>Welcome to react project</h1>
+        <p>The projects are coming soon... </p>
+    </div>
+    </>
+  )
+}
+
+export default App
