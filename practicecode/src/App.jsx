bea39fd1f1a37132ed936ddd94c2d535b313550b
ggstackdeveloper <ggstackdeveloper@gmail.com>
2026-09-01 22:08:56 +0530

    minor changes with comps added

--- practicecode/src/App.jsx
+++ practicecode/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header'
+
+
 
 
 function App() {
@@ -8,6 +11,12 @@ function App() {
       <div className="container">
         <h1>Welcome to react project</h1>
         <p>The projects are coming soon... </p>
+        <div>This is header component 
+          <Header />
+         
+          
+        </div>
+        <div>This is main content component</div>
     </div>
     </>
   )
---
+++ practicecode/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div><h3>Header content area discplaying</h3></div>
+  )
+}
+
+export default Header
